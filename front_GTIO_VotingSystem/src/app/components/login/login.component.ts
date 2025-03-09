6598b67db37a474b028148eb6bbc575b7a8eecd9
Mayra <nogales.127051@e.unavarra.es>
2025-03-09 12:39:01 +0100

    #45 #46 login con servicio

--- front_GTIO_VotingSystem/src/app/components/login/login.component.ts
+++ front_GTIO_VotingSystem/src/app/components/login/login.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { LoginDto } from '../../dtos/loginDto';
+import { LoginService } from '../../services/loginService';
 @Component({
   selector: 'app-login',
   imports: [CommonModule, ReactiveFormsModule],
@@ -11,13 +17,53 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
-  login(event: Event) {
-    event.preventDefault();
-    //alert('Inicio de sesión exitoso');
-    this.router.navigate(['/demo']);
+  loginForm: FormGroup;
+  errorMessage: string | null = null;
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private service: LoginService // Inyecta el servicio
+  ) {
+    this.loginForm = this.fb.group({
+      nombreUsuario: [''],
+      password: [''],
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      const user: LoginDto = new LoginDto(
+        this.loginForm.value.nombreUsuario,
+        this.loginForm.value.password
+      );
+
+      /*this.service.login(user).subscribe({
+        next: (response) => {
+          alert('Inicio de sesión exitoso');
+          console.log('Respuesta del servidor:', response);
+          this.router.navigate(['/demo']);
+        },
+        error: (err) => {
+          this.errorMessage = 'Usuario o contraseña incorrectos';
+          console.error('Error:', err);
+        },
+      });*/
+      this.router.navigate(['/demo']);
+    } else {
+      alert('Por favor, ingrese datos válidos');
+    }
   }
   goToRegister() {
     this.router.navigate(['/register']);
   }
+  //#region Form methods
+  get nombreUsuario() {
+    return this.loginForm.get('nombreUsuario');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+  //#endregion
 }
---
+++ front_GTIO_VotingSystem/src/app/dtos/loginDto.ts
@@ -0,0 +1,3 @@
+export class LoginDto {
+  constructor(public nombreUsuario: string, public password: string) {}
+}
