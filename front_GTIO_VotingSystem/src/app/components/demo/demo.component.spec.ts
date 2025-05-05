00b567166f032c69382a5f6bcae9b0bd160b81df
ivarela22 <90469480+ivarela22@users.noreply.github.com>
2025-05-05 20:03:37 +0200

    Update demo.component.spec.ts

--- front_GTIO_VotingSystem/src/app/components/demo/demo.component.spec.ts
+++ front_GTIO_VotingSystem/src/app/components/demo/demo.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing'; 
 import { DemoComponent } from './demo.component';
 
 describe('DemoComponent', () => {
@@ -8,9 +8,11 @@ describe('DemoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DemoComponent]
-    })
-    .compileComponents();
+      imports: [
+        HttpClientTestingModule,
+        DemoComponent
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DemoComponent);
     component = fixture.componentInstance;
