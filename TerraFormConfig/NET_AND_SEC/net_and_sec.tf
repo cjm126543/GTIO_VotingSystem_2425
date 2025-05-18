d3f4a462f40c40f6b0ff30dc868be5673fc6ffab
ivarela22 <90469480+ivarela22@users.noreply.github.com>
2025-05-18 10:35:43 +0200

    Update net_and_sec.tf

--- TerraFormConfig/NET_AND_SEC/net_and_sec.tf
+++ TerraFormConfig/NET_AND_SEC/net_and_sec.tf
@@ -1,8 +1,4 @@
-// Create desired inbound rules
 resource "aws_security_group_rule" "sql_in_rule" {
-  create_before_destroy = true
-  prevent_destroy       = false
-  ignore_changes        = [cidr_blocks]
   type              = "ingress"
   from_port         = 1433
   to_port           = 1433
@@ -10,12 +6,15 @@ resource "aws_security_group_rule" "sql_in_rule" {
   cidr_blocks       = ["10.0.0.0/16"]
   security_group_id = data.aws_security_group.default_sec_grp.id
   description       = "SQL Server in rule"
+
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy       = false
+    ignore_changes        = [cidr_blocks]
+  }
 }
 
 resource "aws_security_group_rule" "front_in_rule" {
-  create_before_destroy = true
-  prevent_destroy       = false
-  ignore_changes        = [cidr_blocks]
   type              = "ingress"
   from_port         = 4200
   to_port           = 4200
@@ -23,12 +22,15 @@ resource "aws_security_group_rule" "front_in_rule" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = data.aws_security_group.default_sec_grp.id
   description       = "Frontend in rule"
+
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy       = false
+    ignore_changes        = [cidr_blocks]
+  }
 }
 
 resource "aws_security_group_rule" "back_in_rule" {
-  create_before_destroy = true
-  prevent_destroy       = false
-  ignore_changes        = [cidr_blocks]
   type              = "ingress"
   from_port         = 8080
   to_port           = 8080
@@ -36,12 +38,15 @@ resource "aws_security_group_rule" "back_in_rule" {
   cidr_blocks       = ["10.0.0.0/16"]
   security_group_id = data.aws_security_group.default_sec_grp.id
   description       = "backend in rule"
+
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy       = false
+    ignore_changes        = [cidr_blocks]
+  }
 }
 
 resource "aws_security_group_rule" "kong_in_rule" {
-  create_before_destroy = true
-  prevent_destroy       = false
-  ignore_changes        = [cidr_blocks]
   type              = "ingress"
   from_port         = 8000
   to_port           = 8000
@@ -49,4 +54,10 @@ resource "aws_security_group_rule" "kong_in_rule" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = data.aws_security_group.default_sec_grp.id
   description       = "kong in rule"
+
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy       = false
+    ignore_changes        = [cidr_blocks]
+  }
 }
